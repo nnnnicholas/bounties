97196f117399bf029370492cddea7e7ac5bc12a0
nicholas <nnnnicholas@users.noreply.github.com>
2022-02-01 01:42:30 -0500

    started tests

--- src/test/Bounties.sol
+++ src/test/Bounties.sol
@@ -2,11 +2,19 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "ds-test/test.sol";
+import "../Bounties.sol";
 
 contract BountiesTest is DSTest {
-    function setUp() public {}
+    Bounties bounty;
+    function setUp() public {
+        bounty = new Bounties();
+    }
 
     function testExample() public {
         assertTrue(true);
+    }
+
+    function testFailExample() public {
+        assertTrue(false);
     }
 }
